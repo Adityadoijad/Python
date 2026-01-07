2092ec28280e9b5735ab745b8ac368f8b75b5598
Adityadoijad <adityadoijad29@gmail.com>
2026-01-07 19:34:06 +0530

    For Loop

---
+++ Tutorial11.py
@@ -0,0 +1,31 @@
+# FOR LOOP
+
+for i in range(5):
+    print(i+1)
+print("\n")
+
+name="AdityaDoijad"
+for k in name:
+    print(k)
+    if (k== "a"):
+     print("\n")
+print("\n")
+
+
+colours=["red","yellow","green","black"]
+for colour in colours:
+   print(colour)
+   for k in colour:
+      print(k)
+print("\n")
+
+for j in range(1,5):
+   print(j)
+print("\n")
+
+
+''' 
+Next Program is about step range in for loop(start,stop,step)
+'''
+for l in range(0,20,3):  
+   print(l)
